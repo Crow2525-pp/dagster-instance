cafec6bc7c2c45a892aefd1fa8387aaa21a79014
Phil Patterson <philip.james.patterson@gmail.com>
2024-07-12 20:04:07 +1000

    Oh man I'm close to getting this done!

--- pipeline_personal_finance/__init__.py
+++ pipeline_personal_finance/__init__.py
@@ -11,16 +11,14 @@ load_dotenv()
 
 my_logger = get_dagster_logger()
 
-# Ensure EnvVar values are properly fetched from the environment variables
-conn_str = URL.create(
-    drivername="postgresql",
-    username=os.environ.get("DAGSTER_POSTGRES_USER"),
-    password=os.environ.get("DAGSTER_POSTGRES_PASSWORD"),
-    host=os.environ.get("DAGSTER_POSTGRES_HOST"),
-    #port=int(os.environ.get("DAGSTER_POSTGRES_PORT")),
-    database=os.environ.get("DAGSTER_POSTGRES_DB"),
-)
-my_logger.info(f"Connection string: {conn_str}")
+my_logger = get_dagster_logger()
+
+# Debugging: Print environment variables to verify they are loaded
+my_logger.info(f"DAGSTER_POSTGRES_HOST: {os.environ.get('DAGSTER_POSTGRES_HOST')}")
+my_logger.info(f"DAGSTER_POSTGRES_USER: {os.environ.get('DAGSTER_POSTGRES_USER')}")
+my_logger.info(f"DAGSTER_POSTGRES_PASSWORD: {os.environ.get('DAGSTER_POSTGRES_PASSWORD')}")
+my_logger.info(f"DAGSTER_POSTGRES_PORT: {os.environ.get('DAGSTER_POSTGRES_PORT')}")
+my_logger.info(f"DAGSTER_POSTGRES_DB: {os.environ.get('DAGSTER_POSTGRES_DB')}")
 
 @resource
 def sqlalchemy_resource(context):
